e3504e916f2266abbdce1ec59a483eda2955f56d
diabo23 <125207090+diabo23@users.noreply.github.com>
2024-12-18 23:25:34 +0100

    Add files via upload

--- 6-iac-installation.sh
+++ 6-iac-installation.sh
@@ -7,11 +7,25 @@ echo "
 ╚═╝╚═╝  ╚═╝ ╚═════╝    ╚═╝      ╚═════╝╚══════╝     ╚═════╝╚══════╝╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
 "
 
-#Variables specific to download components via API (in our case, FCS CLI Tool in IAC Section)
-export FALCON_API_HOST=api.crowdstrike.com
+if [ "$CROWDSTRIKE_CLOUD_ENV" == "us-1" ]
+then
+  FALCON_API_HOST=api.crowdstrike.com
+elif [ "$CROWDSTRIKE_CLOUD_ENV" == "us-2" ]
+then
+  FALCON_API_HOST=api.us-2.crowdstrike.com
+elif [ "$CROWDSTRIKE_CLOUD_ENV" == "eu-1" ]
+then
+  FALCON_API_HOST=api.eu-1.crowdstrike.com
+else
+  read -p "CrowdStrike Falcon API Hostname: " FALCON_API_HOST
+fi
+
+#US-1: api.crowdstrike.com
+#US-2: api.us-2.crowdstrike.com
+#EU-1: api.eu-1.crowdstrike.com
+
 export FALCON_API_TOKEN_URL=https://$FALCON_API_HOST/oauth2/token
 
-#Get an API Access Token and assign it to a variable
 export FALCON_API_TOKEN=$(curl -X POST --silent \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --data-urlencode "client_id=${FALCON_CLIENT_ID}" \
@@ -20,23 +34,20 @@ export FALCON_API_TOKEN=$(curl -X POST --silent \
 
 export FALCON_FILE_ENUMERATION_URL=https://$FALCON_API_HOST/csdownloads/entities/files/enumerate/v1
 
-#Assign the file_name variable
 export IAC_FILE_NAME=$(curl -X GET --silent \
-	--header "Authorization: Bearer ${FALCON_API_TOKEN}" \
-	--header "Accept: application/json" \
-	--header "Content-Type: application/json" \
-	"${FALCON_FILE_ENUMERATION_URL}" \
-	| jq -r 'last(.resources[] | select(.platform == "linux-amd64").file_name)')
+  --header "Authorization: Bearer ${FALCON_API_TOKEN}" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  "${FALCON_FILE_ENUMERATION_URL}" \
+  | jq -r 'last(.resources[] | select(.platform == "linux-amd64").file_name)')
 
-#Assign the version variable
 export IAC_FILE_VERSION=$(curl -X GET --silent \
-	--header "Authorization: Bearer ${FALCON_API_TOKEN}" \
-	--header "Accept: application/json" \
-	--header "Content-Type: application/json" \
-	"${FALCON_FILE_ENUMERATION_URL}" \
-	| jq -r 'last(.resources[] | select(.platform == "linux-amd64").version)')
+  --header "Authorization: Bearer ${FALCON_API_TOKEN}" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  "${FALCON_FILE_ENUMERATION_URL}" \
+  | jq -r 'last(.resources[] | select(.platform == "linux-amd64").version)')
 
-#Get a pre-signed URL to download the file
 export FALCON_FILES_DOWNLOAD_URL=https://$FALCON_API_HOST/csdownloads/entities/files/download/v1
 
 export IAC_AUTH_URL=$(curl -X GET --silent -G \
@@ -47,19 +58,12 @@ export IAC_AUTH_URL=$(curl -X GET --silent -G \
   --data-urlencode "file_version=${IAC_FILE_VERSION}" \
   "${FALCON_FILES_DOWNLOAD_URL}" | jq -r '.resources.download_url')
 
-#Download the FCS CLI Tool
 wget -O fcs-cli-tool.tar.gz $IAC_AUTH_URL
-
-#Unpack the archive
 tar -xvzf fcs-cli-tool.tar.gz
-
-#Make the file executable
 chmod +x fcs
 
-#Create the folder to store the FCS CLI Profile
 mkdir -p ~/.crowdstrike
 
-#Create the FCS CLI Profile
 cat > ~/.crowdstrike/fcs_profiles.json <<EOF
 {
     "default": {
