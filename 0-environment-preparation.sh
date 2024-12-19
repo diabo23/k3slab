2748d46f5d9a0905544e310cef7374e64e6420a6
diabo23 <125207090+diabo23@users.noreply.github.com>
2024-12-19 21:55:54 +0100

    Add files via upload

--- 0-environment-preparation.sh
+++ 0-environment-preparation.sh
@@ -1,25 +1,19 @@
-echo "
-██╗  ██╗██████╗ ███████╗    ██╗      █████╗ ██████╗     ██████╗ ██████╗ ███████╗██████╗  █████╗ ██████╗  █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
-██║ ██╔╝╚════██╗██╔════╝    ██║     ██╔══██╗██╔══██╗    ██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
-█████╔╝  █████╔╝███████╗    ██║     ███████║██████╔╝    ██████╔╝██████╔╝█████╗  ██████╔╝███████║██████╔╝███████║   ██║   ██║██║   ██║██╔██╗ ██║
-██╔═██╗  ╚═══██╗╚════██║    ██║     ██╔══██║██╔══██╗    ██╔═══╝ ██╔══██╗██╔══╝  ██╔═══╝ ██╔══██║██╔══██╗██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
-██║  ██╗██████╔╝███████║    ███████╗██║  ██║██████╔╝    ██║     ██║  ██║███████╗██║     ██║  ██║██║  ██║██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
-╚═╝  ╚═╝╚═════╝ ╚══════╝    ╚══════╝╚═╝  ╚═╝╚═════╝     ╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
-"
-
 #Prevent Kernel update
 export LINUX_IMAGE=$(dpkg --list | grep linux-image | head -1 | awk '{ print $2 }')
 export LINUX_HEADERS=$(dpkg --list | grep linux-headers | head -1 | awk '{ print $2 }')
 sudo apt-mark hold $LINUX_IMAGE $LINUX_HEADERS linux-image-aws linux-headers-aws
 
-#Fetches the latest version of the package list
-sudo apt-get update
+#######################
+# DOCKER INSTALLATION #
+#######################
 
 #Uninstall all Docker conflicting packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg -y; done
 
-# Add Docker's official GPG key:
+#Fetches the latest version of the package list
 sudo apt-get update
+
+# Add Docker's official GPG key:
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -38,18 +32,38 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 #Allow the use of Docker without sudo (exit or newgrp docker or reboot are required to activate the change)
 sudo usermod -aG docker ${USER}
 
-#Install Helm
+#####################
+# HELM INSTALLATION #
+#####################
+
+sudo apt-get install -y snapd
 sudo snap install helm --classic
 
-#Add CrowdStrike Falcon Helm Chart Repository
+################################################
+# ADD CROWDSTRIKE FALCON HELM CHART REPOSITORY #
+################################################
+
 helm repo add crowdstrike https://crowdstrike.github.io/falcon-helm
 helm repo update
 helm repo list
 
-#Install JQ
-sudo apt install -y jq
+###################
+# JQ INSTALLATION #
+###################
 
-#Install K3s
+sudo apt-get install -y jq
+
+#####################
+# TREE INSTALLATION #
+#####################
+
+sudo apt-get install tree
+
+####################
+# K3S INSTALLATION #
+####################
+
+#K3S Deployment
 curl -sfL https://get.k3s.io | sh -
 
 #Allow the use of kubectl without sudo
@@ -60,14 +74,18 @@ sudo k3s kubectl config view --raw > "$KUBECONFIG"
 #Make the change persistent at reboot
 echo export KUBECONFIG="/home/$USER/.kube/config" >> ~/.bash_profile
 
-#Enable Auto-Completion for Kubectl
-echo 'source <(kubectl completion bash)' >>~/.bash_profile
+#Enable kubectl autocompletion
+echo 'source <(kubectl completion bash)' >> ~/.bash_profile
 
-#Enable the changes in the current shell (not valid after the "newgrp docker" command at the end)
+#Apply the changes
 source ~/.bash_profile
 
-#Avoid the “Kubernetes configuration file is group-readable. This is insecure.” error when installing Helm Chart
+#Avoid a warning from Helm related to configuration file permissions
 chmod 600 ~/.kube/config
+
+#########################
+# SET HOSTNAME AND FQDN #
+#########################
 
 #Set the hostname
 sudo hostnamectl set-hostname ubuntu-box
@@ -78,9 +96,9 @@ sudo bash -c 'echo "127.0.0.1 ubuntu-box.k3s.lab ubuntu-box" >>/etc/hosts'
 #Set the SERVER_FQDN variable
 export SERVER_FQDN=$(hostname -f)
 
-#Download the CrowdStrike's script to download the different components of CrowdStrike CNAPP and make it executable
-curl -sSL -o ~/falcon-container-sensor-pull.sh "https://raw.githubusercontent.com/CrowdStrike/falcon-scripts/main/bash/containers/falcon-container-sensor-pull/falcon-container-sensor-pull.sh"
-chmod +x ~/falcon-container-sensor-pull.sh
+##############################################################
+# DOWNLOAD CROWDSTRIKE SCRIPT TO LIST AND DOWNLOAD RESOURCES #
+##############################################################
 
-#Change the Group ID to immediately activate the use of Docker without sudo (otherwise exit or reboot would be required)
-#newgrp docker
+curl -sSL -o falcon-container-sensor-pull.sh "https://raw.githubusercontent.com/CrowdStrike/falcon-scripts/main/bash/containers/falcon-container-sensor-pull/falcon-container-sensor-pull.sh"
+chmod +x falcon-container-sensor-pull.sh
