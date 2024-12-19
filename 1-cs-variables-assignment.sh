989996a969205622d318a38307aa2ae4f5564e03
diabo23 <125207090+diabo23@users.noreply.github.com>
2024-12-19 22:05:12 +0100

    Add files via upload

--- 1-cs-variables-assignment.sh
+++ 1-cs-variables-assignment.sh
@@ -9,7 +9,7 @@ echo "
 
 #The script needs 4 parameters given to it
 # - The parameters could be given to it in an interactive way; this happens if you run the script with no parameters and you'll have to provide it one by one.
-# - As an alternative, you can run the parameters giving the first three parameters (CID, CrowdStrike Cloud Region and API Client ID); in this case the script will just ask you for the API Client Secret
+# - As an alternative, you can run the parameters giving the first three parameters (CID, CrowdStrike Cloud Region and API Client ID) separated by a blank space; in this case the script will just ask you for the API Client Secret
 #Given the sensitivity of the API Client Secret, while you enter it or while you paste it, the text won't appear in the terminal; once provided, just press Enter to pass it to the script.
 
 export FALCON_CID
@@ -30,19 +30,20 @@ if [ $# -eq 3 ]
                 read -s -p "CrowdStrike API Client Secret: " FALCON_CLIENT_SECRET
 fi
 
+#Get the username we'll use to connect to CrowdStrike Registry
 export FALCON_ART_USERNAME=$(./falcon-container-sensor-pull.sh \
   -u $FALCON_CLIENT_ID \
   -s $FALCON_CLIENT_SECRET \
   --dump-credentials \
   | grep "CS Registry Username" | awk -F ": " '{print $2}')
 
+#Get the password we'll use to connect to CrowdStrike Registry
 export FALCON_ART_PASSWORD=$(./falcon-container-sensor-pull.sh \
   -u $FALCON_CLIENT_ID \
   -s $FALCON_CLIENT_SECRET \
   --dump-credentials \
   | grep "CS Registry Password" | awk -F ": " '{print $2}')
 
+#Get the Token we'll use to pull images from CrowdStrike Registry (this Token does not expire)
 export PARTIALPULLTOKEN=$(echo -n "$FALCON_ART_USERNAME:$FALCON_ART_PASSWORD" | base64 -w 0)
 export FALCON_IMAGE_PULL_TOKEN=$(echo "{\"auths\":{\"registry.crowdstrike.com\":{\"auth\":\"$PARTIALPULLTOKEN\"}}}" | base64 -w 0)
-
-#source ~/.bash_profile
