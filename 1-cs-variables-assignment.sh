9f20ed26503812bd904a18149fc0a7d30ee5dd34
Ubuntu <ubuntu@ubuntu-box.k3s.lab>
2025-01-14 15:32:42 +0000

    - Removed the silent pass of the variables - Added API Client Key as the 4th parameter to be passed

--- 1-cs-variables-assignment.sh
+++ 1-cs-variables-assignment.sh
@@ -9,25 +9,24 @@ echo "
 
 #The script needs 4 parameters given to it
 # - The parameters could be given to it in an interactive way; this happens if you run the script with no parameters and you'll have to provide it one by one.
-# - As an alternative, you can run the parameters giving the first three parameters (CID, CrowdStrike Cloud Region and API Client ID) separated by a blank space; in this case the script will just ask you for the API Client Secret
-#Given the sensitivity of the API Client Secret, while you enter it or while you paste it, the text won't appear in the terminal; once provided, just press Enter to pass it to the script.
+# - As an alternative, you can run the parameters giving the first three parameters (CID, CrowdStrike Cloud Region, API Client ID, API Client Secret) separated by a blank space.
 
 export FALCON_CID
 export CROWDSTRIKE_CLOUD_ENV
 export FALCON_CLIENT_ID
 export FALCON_CLIENT_SECRET
 
-if [ $# -eq 3 ]
+if [ $# -eq 4 ]
         then
                 FALCON_CID=$1
                 CROWDSTRIKE_CLOUD_ENV=$2
                 FALCON_CLIENT_ID=$3
-                read -s -p "CrowdStrike API Client Secret: " FALCON_CLIENT_SECRET
+                FALCON_CLIENT_SECRE=$4
         else
                 read -p "CrowdStrike CID: " FALCON_CID
                 read -p "CrowdStrike Cloud Region: " CROWDSTRIKE_CLOUD_ENV
                 read -p "CrowdStrike API Client Key: " FALCON_CLIENT_ID
-                read -s -p "CrowdStrike API Client Secret: " FALCON_CLIENT_SECRET
+                read -p "CrowdStrike API Client Secret: " FALCON_CLIENT_SECRET
 fi
 
 #Get the username we'll use to connect to CrowdStrike Registry
