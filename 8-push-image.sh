409eb74ab7c89b1c7125289b2472d67b8211a0f7
diabo23 <125207090+diabo23@users.noreply.github.com>
2025-02-07 11:43:08 +0100

    Update 8-push-image.sh

--- 8-push-image.sh
+++ 8-push-image.sh
@@ -5,9 +5,13 @@ export PR_REPOSITORY
 export PR_TAG
 
 read -p "Tag of the Public Image to Pull (e.g. hello-world:linux): " PUB_IMAGE
+
 read -p "Docker Hub Private Registry/Username: " PR_REGISTRY
+
 read -p "Docker Hub Token: " PR_PASSWORD
+
 read -p "Target Repository in the Private Registry: " PR_REPOSITORY
+
 read -p "Tag to assign to the Image (e.g. hello-world-1): " PR_TAG
 
 docker pull $PUB_IMAGE
